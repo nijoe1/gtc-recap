ed4321583241e604cae6d49e5cac265d79ffcc19
0xKurt <Kurt@Merbeth.io>
2024-12-10 13:52:42 +0100

    add og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <GlobalHead />
       <body>
-        <div className="min-h-screen bg-gradient-to-b from-purple-50 to-pink-50">
+        <div className="min-h-screen bg-[#F5F4FE]">
           <header className="border-b">
             <div className="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
               <Link href="/" className="flex items-center gap-2">
