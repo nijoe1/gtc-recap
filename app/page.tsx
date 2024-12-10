b697649356c1a5f77ea8a74047c3197160429332
Nick Lionis <nikolaos@gitcoin.co>
2024-12-10 17:25:03 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,50 @@ import { isValidEthereumAddress, isValidENS } from "@/lib/utils";
 import { useToast } from "@/hooks/use-toast";
 import SliderWithData from "@/components/SliderWithData";
 
+const getMetadata = async() => {
+  return "https://gateway.pinata.cloud/ipfs/bafybeiascfaj7n4zt37xhf6pphstmf6cyhvbvia4oa4tfjluj2rnk67g6i";
+}
+
+export async function generateMetadata(){
+  const image = await getMetadata();
+
+  return {
+  title: "Gitcoin | Recap 2024",
+  description: "Discover Your Gitcoin Impact",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    type: "website",
+    url: "https://recap.gitcoin.co",
+    title: "Gitcoin | Recap 2024",
+    description: "Discover Your Gitcoin Impact",
+    siteName: "Gitcoin",
+    images: [
+      {
+        url: image,
+        width: 1200,
+        height: 564,
+        alt: "Gitcoin Recap 2024",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@Gitcoin",
+    creator: "@Gitcoin",
+    title: "Gitcoin | Recap 2024",
+    description: "Discover Your Gitcoin Impact",
+    images: [
+      {
+        url: image,
+        alt: "Gitcoin Recap 2024",
+      },
+    ],
+  },
+};
+}
+
 export default function Home() {
   const [address, setAddress] = useState("");
   const [status, setStatus] = useState<RecapStatus>("idle");
